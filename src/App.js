6ca4a61b4f6955f7fdd81beca4a30f06451cc4d1
SOFIA <rusofiaru@gmail.com>
2021-01-18 22:07:13 +0200

    beautify

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react'
-//import './App.css'
-import { useDispatch } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { initGloves } from './reducers/glovesReducer'
+import React, {useEffect} from 'react'
+import './App.css'
+import {useDispatch} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {initGloves} from './reducers/glovesReducer'
 import Header from './components/header'
-import { initBeanies } from './reducers/beaniesReducer'
+import {initBeanies} from './reducers/beaniesReducer'
 import Switcher from './components/switcher'
-import { initMasks } from './reducers/facemasksReducer'
+import {initMasks} from './reducers/facemasksReducer'
 
 function App() {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(initGloves())
-    dispatch(initBeanies())
-    dispatch(initMasks())
-  }, [dispatch])
-  return (
-    <div className="App">
-      <Router>
-        <Header/>
-        <Switcher/>
-      </Router>
-    </div>
-  )
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(initGloves())
+        dispatch(initBeanies())
+        dispatch(initMasks())
+    }, [dispatch])
+    return (
+        <div className="App">
+            <Router>
+                <Header/>
+                <Switcher/>
+            </Router>
+        </div>
+    )
 }
 
 export default App
