8f1c4caac9b8955d0c5245dc96a6205f9588e6b6
SOFIA <rusofiaru@gmail.com>
2021-01-18 22:33:36 +0200

    add proxy server

--- src/services/availabilityService.js
+++ src/services/availabilityService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/availability'
+const baseUrl = '/api/availability'
 const getAvailability = async (manufacturer, productId) => {
   const response = await axios.get(`${baseUrl}/${manufacturer}/${productId}`)
   return response.data.availability
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/products'
+const baseUrl = '/api/products'
 const getProducts = async (type) => {
   const response = await axios.get(`${baseUrl}/${type}`)
   return response.data
